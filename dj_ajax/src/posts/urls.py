81d4ac1f92de5d629dbd10739466d4138a05a9a6
Amalaviya3030 <aryanmalaviya708@gmail.com>
2025-04-10 05:30:33 -0400

    first of all make the DOM Manipulation then call ajax and then Get Posts Objects with Ajax

--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from .views import(
     posts_list_and_create,
+    load_posts_data_view,
+    hello_world_view
 )
 app_name = 'posts'
 
 urlpatterns = [
-    path('', posts_list_and_create, name='main'),
+    path('', posts_list_and_create, name='main-board'),
+    path('data/', load_posts_data_view, name='posts-data'),
+    path('hello-world/', hello_world_view, name='hello-world'),
 ]
