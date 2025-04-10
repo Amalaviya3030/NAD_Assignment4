68a31f99161348961be2a76cb72d2c908ec7de34
Amalaviya3030 <aryanmalaviya708@gmail.com>
2025-04-10 17:25:16 -0400

    add the spinner and card layout as well add the new Button name is like

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -2,6 +2,8 @@ console.log('hello world')
 
 const helloWolrdBox = document.getElementById('hello-world')
 const postsBox = document.getElementById('posts-box')
+const spinnerBox = document.getElementById('spinner-box')
+
 
 $.ajax({
     type: 'GET',
@@ -19,14 +21,32 @@ $.ajax({
     type: 'GET',
     url: '/data/',
     success: function(response){
-    console. log(response)
-    const data = response.data
-    console.log(data)
-    data.forEach(el => {
-        postsBox.innerHTML += `
-            ${el.title} - <b>${el.body}</b><br>
-        `
-    });
+        console. log(response)
+        const data = response.data
+        setTimeout(()=>{
+            console.log(data)
+            spinnerBox.classList.add('not-visible')
+            data.forEach(el => {
+                postsBox.innerHTML += `
+                    <div class="card mb-2">
+                        <div class="card-body">
+                            <h5 class="card-title">${el.title}</h5>
+                            <p class="card-text">${el.body}</p>
+                        </div>
+                        <div class="card-footer">
+                            <div class="row">
+                                <div class="col-1">
+                                    <a href="#" class="btn btn-primary">Details</a>
+                                </div>
+                                <div class="col-1">
+                                    <a href="#" class="btn btn-primary">Like</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                `
+            });
+        }, 100)
     },
     error: function(error){
     console. log(error)
