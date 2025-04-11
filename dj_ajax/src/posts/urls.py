0a57886ad735bb44f5addc67f05457fe2559ea39
Amalaviya3030 <aryanmalaviya708@gmail.com>
2025-04-10 23:57:54 -0400

    at this point i am add the loadmore butten like butten workig and add the addpost butten

--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from .views import(
     posts_list_and_create,
     load_posts_data_view,
-    hello_world_view
+    hello_world_view,
+    like_unlike_post,
 )
 app_name = 'posts'
 
 urlpatterns = [
     path('', posts_list_and_create, name='main-board'),
-    path('data/', load_posts_data_view, name='posts-data'),
+    path('like-unlike/', like_unlike_post, name='like-unlike'),
+    path('data/<int:num_posts>/', load_posts_data_view, name='posts-data'),
     path('hello-world/', hello_world_view, name='hello-world'),
 ]
